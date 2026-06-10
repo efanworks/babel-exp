6f50ef4b8537d1bd5a207960c75da86becb48dd1
efanworks <erfanvv@163.com>
2026-06-10 15:35:27 +0800

    测试平台

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import { createElement } from "react";
+import { type RouteObject } from "react-router-dom";
+import { Tasks as TasksZustand } from "@efanworks/tasks-with-zustand";
+
+export const routes: RouteObject[] = [
+  { path: "/tasks-zustand", element: createElement(TasksZustand) },
+];
