390481d0bd919d95937459e185eef67f88eff9a5
claytonzhuo <eafanzhuo@foxmail.com>
2026-06-10 12:02:15 +0800

    tasks-with-zustand 打包

--- packages/tasks-with-zustand/tsup.config.ts
+++ packages/tasks-with-zustand/tsup.config.ts
@@ -2,7 +2,16 @@ import { defineConfig } from "tsup";
 
 export default defineConfig(() => ({
   entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
   splitting: false,
   sourcemap: true,
   clean: true,
+  minify: true,
+  // 确保 react 不会被打包
+  external: ["react", "react-dom", "zustand"],
+  outExtension({ format }) {
+    return {
+      js: format === "esm" ? ".mjs" : ".cjs",
+    };
+  },
 }));
