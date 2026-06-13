5401593db83e7ab3e6fbfb5af38c853a86006af0
efanworks <erfanvv@163.com>
2026-06-13 16:37:03 +0800

    修复tasks-with-zustand 数据获取失败问题

--- packages/tasks-with-zustand/src/components/TaskList/index.tsx
+++ packages/tasks-with-zustand/src/components/TaskList/index.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import { useTasks } from "../../store/useTasks";
 import { TaskItem } from "../TaskItem";
 
 export const TaskList = () => {
   const tasks = useTasks((state) => state.tasks);
+  const fetchTasks = useTasks((state) => state.fetchTasks);
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
   return (
     <ul>
